dc164cdfd5fb8bd53f3e86ba37f6922e2ea09d5a
Liam H <liam.h.-@hotmail.com>
2018-11-05 15:30:11 +0000

    Added file io package to handle reads and writes

---
+++ src/lemacs/io/FileHandler.java
@@ -0,0 +1,13 @@
+package lemacs.io;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+
+
+public class FileHandler{
+
+	
+}
