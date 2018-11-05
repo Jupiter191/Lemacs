f3273480379a2d096f06caf51105f3391e295901
Liam H <liam.h.-@hotmail.com>
2018-11-05 14:26:41 +0000

    Added additional WIP file IO code

--- src/lemacs/Lemacs.java
+++ src/lemacs/Lemacs.java
@@ -2,24 +2,41 @@ package lemacs;
 
 import lemacs.view.View;
 import lemacs.model.Model;
+import lemacs.io.FileHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Lemacs{
 
 	private static final Lemacs lemacs = new Lemacs();
 	private static final View view = new View();
 	private static final Model model = new Model();
+	private static BufferedReader buffReader;
 
 	public static void main(String[] args){
 
 		if(args.length == 0){
-			view.initialiseGUI();
+			lemacs.newFile();
 		} else {
 			lemacs.openFile();
 		}
 	}
 
 
+	private void newFile(){
+		
+	}
+
+
 	private void openFile(){
+		try {
+			throw new IOException();			
+		} catch(IOException e){
+			//view.alert(e); // alert overload that takes an Exception
+		}
 		System.out.println("NO ARGS - openFile() called");
 	}
 }
